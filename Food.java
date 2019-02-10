ac1d342347166083d6e098cab3eed9d667e5cbf2
Rayan Ahmed <rahmed@ltsp22.udallas.edu>
2019-02-09 21:30:40 -0600

    Added a food object with attributes open to discussion

---
+++ Food.java
@@ -0,0 +1,30 @@
+package restaurantObjects;
+
+public class Food {
+
+    enum FoodType {
+        BreakFast, Lunch, Dinner, AllDay;
+    }
+
+    private FoodType foodType;
+    private String name;
+    private double price;
+
+    // since foodType is an enum, make sure when instantiating a food object, you do FoodType.valueOf("some_foodType as string")
+    //cannot change foodType once updated
+    public Food(String name, FoodType foodType, Double Price) {
+        this.foodType = foodType;
+        this.name = name;
+        this.priece = price;
+    }
+    
+    //public setters
+    public void setName(String name) {this.name = name;}
+    public void setPrice(double price) {this.price = price;}
+
+    //public getters
+    public String getName() {return this.name;}
+    public String getPrice() {return this.price + "$";}
+    
+
+}
